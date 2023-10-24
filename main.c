6c0ee0e3511996742e91f26a2d96c46a839d5b51
Herbert Gerardo Mendoza Paredes <130772923+Midnight024-ai@users.noreply.github.com>
2023-10-23 23:14:25 -0500

    Funciones principales

--- main.c
+++ main.c
@@ -18,3 +18,57 @@ typedef struct {
     Bucket table[TABLE_SIZE];
 } HashTable;
 
+Node *create_node(int value) {
+    Node *newNode = malloc(sizeof(Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+HashTable *init_table(int (*hashFunc)(int)) {
+    HashTable *ht = malloc(sizeof(HashTable));
+    ht->hashFn = hashFunc;
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        ht->table[i].head = NULL;
+    }
+    return ht;
+}
+
+void add_value(HashTable *ht, int value) {
+    int index = ht->hashFn(value);
+    Node *newNode = create_node(value);
+    newNode->next = ht->table[index].head;
+    ht->table[index].head = newNode;
+}
+
+int search_value(HashTable *ht, int value) {
+    int index = ht->hashFn(value);
+    Node *current = ht->table[index].head;
+    while (current) {
+        if (current->data == value) {
+            return current->data;
+        }
+        current = current->next;
+    }
+    return -1;
+}
+
+void free_table(HashTable *ht) {
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        Node *current = ht->table[i].head;
+        while (current) {
+            Node *temp = current;
+            current = current->next;
+            free(temp);
+        }
+    }
+    free(ht);
+}
+
+int modHash(int value) {
+    return value % TABLE_SIZE;
+}
+
+int randomHash(int value) {
+    return rand() % TABLE_SIZE;
+}
